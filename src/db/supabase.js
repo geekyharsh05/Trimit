05df38c9c4dbda8290d2babba12f17babddf5002
geekyharsh05 <hp424805@gmail.com>
2024-06-27 14:10:48 +0530

    chore: Update Supabase configuration to use environment variables

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_KEY, SUPABASE_URL } from "@/utils/envConfigs";
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabaseUrl = SUPABASE_URL;
+const supabaseKey = SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
---
+++ src/utils/envConfigs.js
@@ -0,0 +1,3 @@
+export const PUBLIC_BASE_URL = import.meta.env.VITE_PUBLIC_BASE_URL;
+export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
