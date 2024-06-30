d3949cee24ca28cf0f646fe15529b804844c618c
geekyharsh05 <hp424805@gmail.com>
2024-06-30 22:22:09 +0530

    chore: Update Footer component with GitHub contribute button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
+import Github from "./Icons/Github";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white mt-10 border-t border-gray-700">
-      <div className="container mx-auto py-10 px-5 text-center">
+      <div className="container mx-auto py-7 px-5 text-center">
+        <div className="flex items-center justify-center mb-3 p-t">
+          <button
+            onClick={() =>
+              window.open("https://github.com/geekyharsh05/Trimit", "_blank")
+            }
+            type="button"
+            className="flex items-center justify-center gap-x-2 rounded-full border border-slate-600 bg-transparent py-3 px-4 text-slate-300 transition hover:text-indigo-500 sm:w-auto font-extralight"
+          >
+            <Github className="h-5 w-5 sm:h-6 sm:w-6" />
+            <span className="hidden sm:inline">Contribute Now!</span>
+            <span className="inline sm:hidden">Contribute</span>
+          </button>
+        </div>
         <p>
           Made with ❤️ by{" "}
           <Link to="/" className="text-blue-500 hover:text-blue-400">
