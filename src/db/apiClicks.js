063b223a4bfdab757ff9bae3e221404d97322c7b
geekyharsh05 <hp424805@gmail.com>
2024-07-07 20:40:14 +0530

    feat: Update device type default value in storeClicks function

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -34,7 +34,7 @@ const parser = new UAParser();
 export const storeClicks = async ({ id, originalUrl }) => {
   try {
     const res = parser.getResult();
-    const device = res.type || "desktop"; // Default to desktop if type is not detected
+    const device = res.device.type || "desktop"; // Default to desktop if type is not detected
 
     const response = await fetch("https://ipapi.co/json");
     const { city, country_name: country } = await response.json();
